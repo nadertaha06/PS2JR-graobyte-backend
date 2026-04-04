41b743715af395e000954a8ebb18fae769802844
Nader Taha <tahanader068@gmail.com>
2026-04-04 01:34:25 -0300

    chore: remove Flask debug configuration from .env.example and update run.py to use environment variable for debug mode

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    debug = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
-    app.run(debug=debug)
+    app.run(host='0.0.0.0', debug=False)
